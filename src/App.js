61a8bac5be5a45a8011403b06c7ac4c8916fe970
Ayokunnumi1 <omololuayk@gmail.com>
2023-09-13 19:20:16 +0100

    Create calculator.js component & import into App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
+import CalculatorElement from './components/calculator';
 
 const App = () => (
-  <div className="App" />
+  <div className="App">
+    <CalculatorElement />
+  </div>
 );
 
 export default App;
---
+++ src/components/calculator.js
@@ -0,0 +1,28 @@
+const CalculatorElement = () => (
+  <div className="grid-container">
+    <div className="input-number-container">
+      <input className="input-number" />
+    </div>
+    <div>AC</div>
+    <div>+/-</div>
+    <div>%</div>
+    <div>/</div>
+    <div>7</div>
+    <div>8</div>
+    <div>9</div>
+    <div>X</div>
+    <div>4</div>
+    <div>5</div>
+    <div>6</div>
+    <div>-</div>
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+    <div>+</div>
+    <div className="item-seventeen">0</div>
+    <div className="item-eighteen">.</div>
+    <div>=</div>
+  </div>
+);
+
+export default CalculatorElement;
