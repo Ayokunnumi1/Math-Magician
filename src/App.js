6cb14e0ebbea8aeae97ec5e22cffae3586cda5a5
Ayokunnumi1 <omololuayk@gmail.com>
2023-09-13 22:29:40 +0100

    Create one child component in components folder

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import CalculatorElement from './components/calculator';
 
 const App = () => (
---
+++ src/components/calculatorText.js
@@ -0,0 +1,8 @@
+const Text = () => (
+  <>
+    <h1>Calculator</h1>
+    <h2>Lets do some maths</h2>
+  </>
+);
+
+export default Text;
