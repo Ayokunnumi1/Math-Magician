d67a7037d20d10843f4b040ed5d5413607b14b25
Ayokunnumi1 <omololuayk@gmail.com>
2023-09-21 12:25:36 +0100

    Remove commented lines

--- src/components/calculatorText.js
+++ src/components/calculatorText.js
@@ -1,13 +1,11 @@
 import '../modules/Text.css';
-// eslint-disable-next-line arrow-body-style
-const Text = () => {
-  return (
-    <>
-      <div>
-        <h2 className="text-h2">Let&apos;s do some Maths!</h2>
-      </div>
-    </>
-  );
-};
+
+const Text = () => (
+  <>
+    <div>
+      <h2 className="text-h2">Let&apos;s do some Maths!</h2>
+    </div>
+  </>
+);
 
 export default Text;
